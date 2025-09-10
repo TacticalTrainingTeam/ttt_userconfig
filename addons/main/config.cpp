188ef9560d3b40eb83867ff1595dc855b732465a
Andx667 <andx667@protonmail.com>
2025-09-10 20:12:38 +0200

    add settings

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -3,7 +3,7 @@
 class CfgPatches {
     class ADDON {
         author = AUTHOR;
-        authors[] = {"TacticalTrainingTeam"};
+        authors[] = {"Andx"};
         url = CSTRING(url);
         name = COMPONENT_NAME;
         requiredVersion = REQUIRED_VERSION;
---
+++ addons/userconfig/README.md
@@ -0,0 +1,5 @@
+ttt_userconfig_userconfig
+===================
+
+CBA Settings Userconfig
+<https://github-wiki-see.page/m/CBATeam/CBA_A3/wiki/CBA-Settings-System#userconfig>
